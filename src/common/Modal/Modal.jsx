10386a1afad9c2f671122c066223bb142c608307
Yurii <ivaschenko_u@ukr.net>
2022-07-24 01:22:16 +0300

    add modal

---
+++ src/common/Modal/Modal.jsx
@@ -0,0 +1,53 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { useLockBodyScroll } from 'react-use';
+
+import styles from './Modal.module.css';
+
+const modalRootRef = document.querySelector('#modal-root');
+
+const Modal = ({ closeForm, description }) => {
+  useLockBodyScroll(true);
+
+  useEffect(() => {
+    const onEscPress = e => {
+      if (e.code === 'Escape') {
+        closeForm();
+      }
+    };
+
+    window.addEventListener('keydown', onEscPress);
+
+    return () => {
+      window.removeEventListener('keydown', onEscPress);
+    };
+  }, [closeForm]);
+
+  const handleBackdropClick = e => {
+    if (e.target === e.currentTarget) return closeForm();
+  };
+
+  return createPortal(
+    <div className={styles.backdrop} onClick={handleBackdropClick}>
+      <div className={styles.modal}>
+        <header className={styles.header}>
+          <button
+            className={styles.closeBtn}
+            onClick={closeForm}
+            aria-label="Close"
+          >
+            &times;
+          </button>
+        </header>
+        <div className={styles.content}>
+          <div className={styles.lead}></div>
+
+          {description}
+        </div>
+      </div>
+    </div>,
+    modalRootRef,
+  );
+};
+
+export default Modal;
--- src/components/Menu/Submenu/Submenu.jsx
+++ src/components/Menu/Submenu/Submenu.jsx
@@ -1,22 +1,36 @@
+import Modal from 'common/Modal/Modal';
+import { useState } from 'react';
 import s from './Submenu.module.css';
 
 const Submenu = ({ date }) => {
+  const [isOpenMod, setisOpenMod] = useState(false);
+
+  const handelOpen = () => {
+    setisOpenMod(true);
+  };
+  const closeForm = () => {
+    setisOpenMod(prev => !prev);
+  };
   return (
-    <div>
-      <ul>
-        {date.map(({ name, price, gramm, one }) => (
-          <li>
-            <button className={s.btn}>
-              {name}
-              <span className={s.price}>
-                {' '}
-                {price} грн / {gramm || one}{' '}
-              </span>
+    <ul className={s.section}>
+      {date.map(({ name, price, gramm, one, description }) => (
+        <li className={s.link}>
+          <div className={s.right_column}>
+            <p>{name}</p>
+            <button className={s.btn} onClick={handelOpen}>
+              Склад
             </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+            {isOpenMod && (
+              <Modal description={description} closeForm={closeForm} />
+            )}
+          </div>
+
+          <p>
+            {price} грн / {gramm || one}
+          </p>
+        </li>
+      ))}
+    </ul>
   );
 };
 
